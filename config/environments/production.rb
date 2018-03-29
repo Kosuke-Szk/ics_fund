51b096a1a1016710a2094f12459f469c2c5b4190
Kosuke-Szk <brave.ksk@gmail.com>
2018-03-29 11:41:45 +0530

    delete GA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  GA.tracker = "UA-113767013-2"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
